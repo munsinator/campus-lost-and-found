416dabd168e3369426292a01587fa3274349e6fa
Eunice Munsi <if25b020@technikum-wien.at>
2026-09-09 14:31:23 +0200

    added tests

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,44 @@
 import Fastify from "fastify";
 import Postgres from "@fastify/postgres";
-
+import { pathToFileURL } from 'node:url';
 import authRoutes from './routes/auth/index.ts';
 import itemRoutes from './routes/items/index.ts';
 
 //Added (pretty) logger to fastify instance
-const fastify = Fastify({
-    logger: {
-        transport:{
-            target: "pino-pretty"
+export function buildFastifyInstance() {
+    const fastify = Fastify({
+        logger: process.env.NODE_ENV === 'test' ? false : {
+            transport:{
+                target: "pino-pretty"
+            }
         }
-    }
-});
+    });
 
-fastify.register(Postgres, {  
 
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    ssl: {
-        rejectUnauthorized: false
-    } 
-});
+    fastify.register(Postgres, {
 
-fastify.register(authRoutes, { prefix: '/auth'});
-fastify.register(itemRoutes, { prefix: '/items'});
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
 
-//To start the server -> server.listen() + port
-async function start() {
-    await fastify.listen({
-        port:3000,
-        host: '0.0.0.0'
-    })
+    fastify.register(authRoutes, { prefix: '/auth'});
+    fastify.register(itemRoutes, { prefix: '/items'});
+    return fastify;
 }
 
-start();
+//To start the server -> server.listen() + port
 
+if (process.argv[1] && import.meta.url === pathToFileURL(process.argv[1]).href){
+    const app = buildFastifyInstance();
+
+    await app.listen({
+        port: 3000,
+        host: '0.0.0.0'
+    });    
+}
